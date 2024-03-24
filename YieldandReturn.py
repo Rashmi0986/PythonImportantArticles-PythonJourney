a0e82c438f038b0a37a3d9e682b6cb6972dbdfea
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-03-24 11:59:56 +0000

    Yield

--- YieldandReturn.py
+++ YieldandReturn.py
@@ -5,4 +5,5 @@ def get_takoballs(orders):
 for food in get_takoballs([4, 6, 8]):
   print(food)
 
+# return adds to delay where in yield is much faster in memory terms .
 # yield vs return with example - https://levelup.gitconnected.com/return-vs-yield-in-python-a-short-comic-f714465a0e92
