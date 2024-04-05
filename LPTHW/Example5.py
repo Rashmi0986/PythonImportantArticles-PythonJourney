5294b4408055b421458d14024651e9ea6b7b6706
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-04-05 05:01:22 +0000

    LPTHW example5.py

---
+++ LPTHW/Example5.py
@@ -0,0 +1,16 @@
+my_name = "test_user"
+my_age = "25"
+my_height = "5.4"
+my_weight = "180"
+
+print(f"Let's Talk about {my_name}")
+print(f"He is {my_height} inches tall")
+print(f"He is of {my_age}")
+
+total = my_age + my_height + my_weight
+
+print(f"if I add my_age:{my_age} my_weight:{my_height}, my_weight: {my_weight} i will get total:{total}")
+
+
+
+      
