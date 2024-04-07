2b259561d8c2c14e40e98cdf7473ddcdd0e2d036
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-04-07 07:52:23 +0000

    added next file

---
+++ Corey Schafer/Excercise14Video23.py
@@ -0,0 +1,10 @@
+import os
+from datetime import datetime
+
+cwd = "/workspaces/PythonImportantArticles-PythonJourney/Corey Schafer"
+for dirpath , dirnames , filenames in os.walk(cwd):
+    print(f'Current Path: {dirpath}')
+    print(f'Current dirname: {dirnames}')
+    print(f'Current filenames: {filenames}')
+
+    
